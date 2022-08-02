bc9bbb898dc2923c4af61ebb99ab1b2c3b90c595
Dominik Diling <dominik.diling@meelogic.com>
2022-08-02 21:18:56 +0200

    s26

--- stories/ButtonB.stories.jsx
+++ stories/ButtonB.stories.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { ButtonB } from '@components';
+import { ButtonB } from 'components';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
